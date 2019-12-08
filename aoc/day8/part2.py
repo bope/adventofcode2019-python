f294f07a23347524105ba864772e2788b1aae9c3
Per Bodin <per.bodin@gmail.com>
2019-12-08 14:38:43 +0100

    Updated day8

--- aoc/day8/part2.py
+++ aoc/day8/part2.py
@@ -2,24 +2,25 @@ from typing import List
 from collections import Counter, defaultdict
 
 
-def solution(input: List[str], size=(25, 6)) -> str:
+def solution(input: str, size=(25, 6)) -> str:
     layer_size = size[0] * size[1]
     layers = len(input) // layer_size
-    ret = [2] * layer_size
+    ret = ''
 
-    for l in range(layers, 0, -1):
-        end = l * layer_size
-        start = end - layer_size
-
-        for i, pixel in enumerate(input[start:end]):
-            if pixel != 2:
-                ret[i] = pixel
-
-    return '\n'.join([''.join([str(i) for i in row]) for row in [ret[r*size[0]:(r+1)*size[0]] for r in range(size[1])]])
+    for y in range(size[1]):
+        for x in range(size[0]):
+            p = '2'
+            for l in range(layers-1, -1, -1):
+                lp = input[(layer_size * l) + (size[0] * y) + x]
+                if lp != '2':
+                    p = lp
+            ret += p
+        ret += '\n'
+    return ret.strip()
 
 
 if __name__ == '__main__':
     with open('inputs/day8.txt') as fd:
-        ret = solution(list(map(int, fd.read().strip())))
+        ret = solution(fd.read().strip())
         ret = ret.replace('2', ' ').replace('0', '.').replace('1', 'X')
         print(ret)
--- tests/test_day8.py
+++ tests/test_day8.py
@@ -2,5 +2,4 @@ from aoc.day8 import part2
 
 
 def test_part2():
-    input = list(map(int, '0222112222120000'))
-    assert part2.solution(input, (2, 2)) == '01\n10'
+    assert part2.solution('0222112222120000', (2, 2)) == '01\n10'
